46e52353a54384e240298e3ce1798998a46850dc
gpsyrou <gspyrou@umich.edu>
2020-07-15 19:16:55 +0100

    update the read me with project discription

--- Code/consumer_complaints.py.py
+++ Code/consumer_complaints.py.py
@@ -1,30 +1,7 @@
 """
 Consumer Complaints
-Data source: 
-    https://www.kaggle.com/selener/consumer-complaint-database
-    https://catalog.data.gov/dataset/consumer-complaint-database
-
-Context
-These are real world complaints received about financial products and services.
- Each complaint has been labeled with a specific product; therefore, 
- this is a supervised text classification problem. With the aim to classify 
- future complaints based on its content, we used different machine learning
- algorithms can make more accurate predictions (i.e., classify the complaint 
- in one of the product categories)
-
-Content
-The dataset contains different information of complaints that customers
- have made about a multiple products and services in the financial sector,
- such us Credit Reports, Student Loans, Money Transfer, etc.
-The date of each complaint ranges from November 2011 to May 2019.
-
-Acknowledgements
-This work is considered a U.S. Government Work. 
-The dataset is public dataset and it was downloaded from
-https://catalog.data.gov/dataset/consumer-complaint-database
-on 2019, May 13.
-
-
+Author: Georgios Spyrou
+Date Last Updated: 15/07/2020
 """
 
 import os
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Multiclass-Text-Classification-on-Consumer-Complaints
+
+Project Description:
+
+The data used in this project contain complaints that have been made by consumers regarding financial services and products (e.g. Student loans, credit reports, etc) in the US between November 2011 and May 2019.
+Each of the complaints is marked to belong under one Product Category. This makes the data ideal for supervised learning purposes, with
+the text (complaint) as the input, and the category that the complaint belongs to as the target variable.
+
+
+Data source: 
+
+This is a public dataset and can be found in the following location:
+https://catalog.data.gov/dataset/consumer-complaint-database
