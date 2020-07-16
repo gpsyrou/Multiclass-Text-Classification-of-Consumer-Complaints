758544a36f9e404e19e162f69ba84f66599a6641
gpsyrou <gspyrou@umich.edu>
2020-07-16 20:19:01 +0100

    add logic to filter out when no text from consumer

--- Code/consumer_complaints.py.py
+++ Code/consumer_complaints.py.py
@@ -13,22 +13,23 @@ import seaborn as sns
 project_dir = 'C:\\Users\\george\\Desktop\\GitHub\\Projects\\Consumer_Complaints'
 os.chdir(project_dir)
 
-cons_complaints_df = pd.read_csv(
+complaints_df = pd.read_csv(
         os.path.join(project_dir, 'Data\cons_complaints_dataset.csv'))
 
 
 
-cons_complaints_df.dtypes
+complaints_df.dtypes
 # The only column that is not text is the ID of the complaint
 
 # Have a look at what columns the dataset contains
-cons_complaints_df.columns
+complaints_df.columns
 
 
 # Identify how many missing values we have per column
-cons_complaints_df.isnull().sum(axis=0)
+complaints_df.isnull().sum(axis=0)
 
-small = cons_complaints_df[0:10000]
+
+# Part 1. Exploratory Data Analysis
 
 # Main purpose of this project is to use the 'Consumer complaint narrative'
 # column, in order to predict to which category (defined by 'Product' column)
@@ -39,12 +40,12 @@ small = cons_complaints_df[0:10000]
 # is necessary.
 
 plt.figure(figsize=(8,10))
-sns.countplot(y=cons_complaints_df['Product'],
-              order = cons_complaints_df['Product'].value_counts().index)
+sns.countplot(y=complaints_df['Product'],
+              order = complaints_df['Product'].value_counts().index)
 plt.title('Number of Observations per Product Category', fontweight="bold")
 
 # Find states that most complaints have been submitted to
-most_complaints = cons_complaints_df[['Complaint ID',
+most_complaints = complaints_df[['Complaint ID',
                     'State']].groupby(['State']).agg(['count'])
 
 most_complaints = most_complaints.sort_values(by=[('Complaint ID','count')],
@@ -56,3 +57,11 @@ sns.barplot(x=most_complaints.index[0:top_n], y=('Complaint ID','count'),
             data = most_complaints[0:top_n])
 plt.ylabel('Number of complaints')
 plt.title('States with the most number of complaints')
+
+
+# Filter the dataset to retain only the rows for which the
+# 'Consumer complaint narrative' column is populated (i.e. we have input
+# from the consumer regarding the complaint that they are submitting)
+compl_w_text = complaints_df[complaints_df
+                                ['Consumer complaint narrative'].notnull()]
+
