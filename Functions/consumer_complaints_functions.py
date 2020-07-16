372c593a5c7c451ac3838ac5d88ea240dc4bed96
gpsyrou <gspyrou@umich.edu>
2020-07-16 20:38:51 +0100

    add plotNumberOfObservationsPerCategory function

--- Functions/consumer_complaints_functions.py
+++ Functions/consumer_complaints_functions.py
@@ -6,3 +6,12 @@ Date Last Updated: 16/07/2020
 File: Contains the functions used for the Consumer Complaints project
 """
 
+import matplotlib.pyplot as plt
+import searborn as sns
+
+def plotNumberOfObservationsPerCategory(input_df):
+    
+    plt.figure(figsize=(8,10))
+    sns.countplot(y=input_df['Product'],
+                  order = input_df['Product'].value_counts().index)
+    plt.title('Number of Observations per Product Category', fontweight="bold")
