954446d381b323117f9d2fb10756b3c477997492
gpsyrou <gspyrou@umich.edu>
2020-07-26 11:05:07 +0100

    update main script

--- Functions/consumer_complaints_functions.py
+++ Functions/consumer_complaints_functions.py
@@ -26,7 +26,7 @@ def plotNumberOfObservationsPerCategory(input_df: pd.core.frame.DataFrame,
     plt.figure(figsize=(8,10))
     sns.countplot(y=input_df[col],
                   order = input_df[col].value_counts().index)
-    plt.title('Number of Observations per Product Category', fontweight="bold")
+    plt.title(f'Number of Observations per {col} Category', fontweight="bold")
     plt.show()
 
 
